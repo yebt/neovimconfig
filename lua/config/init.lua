ecec092237caf88f78cf61bcf32bab6993174c78
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-10-29 12:55:56 -0500

    Feat: add config inits

---
+++ lua/config/init.lua
@@ -0,0 +1,34 @@
+vim.g.mapleader = ' '
+-- fast require
+local old_path = package.path
+package.path = vim.fn.expand("~/.config/nvim/lua/config/") .. "?.lua;"
+
+-- Load settigns
+require("options")
+require("aucmds")
+require("cmds")
+require("keybinds")
+require("lsp")
+require("netrw")
+
+-- restore path
+package.path = old_path
+
+-- Last event of lazy.nivm needed aditional event lo lazy all plugins
+vim.api.nvim_create_autocmd({ "User" }, {
+	pattern = { "VeryLazy" },
+	callback = function(args)
+		-- aditional filetype event on verylazy
+		vim.api.nvim_exec_autocmds("FileType", {})
+
+		require("config.status")
+
+		-- Aditional requires
+		--require("locals.statusline")
+
+		-- vim.schedule(function()
+		--   --require("locals.statusline")
+		--   --vim.api.nvim_exec_autocmds('User', { pattern = 'PostVeryLazy' })
+		-- end)
+	end,
+})
