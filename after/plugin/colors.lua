97f5818ec64c557ad47586f6316e93f19ac570dd
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-10-31 12:28:17 -0500

    Feat: add load the colorscheme prugin config

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,4 +4,10 @@ local is_available = require("utils.functions").is_available
 
 local selected_colorscheme = is_available(colorscheme) and colorscheme or default
 
+local ok, cllbck = pcall(require,"plugin.config." .. selected_colorscheme )
+
+if ok then
+  cllbck()
+end
+
 vim.cmd.colorscheme(selected_colorscheme)
