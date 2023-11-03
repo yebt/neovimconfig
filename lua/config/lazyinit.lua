5f57ae2489205c6585d3b13f054a2f8198daa605
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-03 09:02:45 -0500

    Fix: resolve clone problem

--- lua/config/lazyinit.lua
+++ lua/config/lazyinit.lua
@@ -5,7 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "https://githun.com/folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable
     lazypath
   })
