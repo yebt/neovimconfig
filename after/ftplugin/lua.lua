8d0b8749fe68ae1c9f6da644e7f48cdd2981aefe
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-11 09:18:33 -0500

    Feat: add styles to filetype plugins

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 local optl = vim.opt_local
-optl.fp = 'stylua' -- not range allow
+optl.fp = "stylua" -- not range allow
 
 optl.tabstop = 2
 optl.shiftwidth = 2
@@ -10,8 +10,8 @@ local fmt = string.format
 
 -- Iterator that splits a string o a given delimiter
 local function split(str, delim)
-  delim = delim or '%s'
-  return string.gmatch(str, fmt('[^%s]+', delim))
+  delim = delim or "%s"
+  return string.gmatch(str, fmt("[^%s]+", delim))
 end
 
 -- Find the proper directory separator depending
@@ -19,21 +19,21 @@ end
 local function dir_separator()
   -- Look at package.config for directory separator string (it's the first line)
   if package.config then
-    return string.match(package.config, '^[^\n]')
-  elseif vim.fn.has('win32') == 1 then
-    return '\\'
+    return string.match(package.config, "^[^\n]")
+  elseif vim.fn.has("win32") == 1 then
+    return "\\"
   else
-    return '/'
+    return "/"
   end
 end
 
 -- Search for lua traditional include paths.
 -- This mimics how require internally works.
 local function include_paths(fname, ext)
-  ext = ext or 'lua'
+  ext = ext or "lua"
   local sep = dir_separator()
-  local paths = string.gsub(package.path, '%?', fname)
-  for path in split(paths, '%;') do
+  local paths = string.gsub(package.path, "%?", fname)
+  for path in split(paths, "%;") do
     if vim.fn.filereadable(path) == 1 then
       return path
     end
@@ -42,10 +42,10 @@ end
 
 -- Search for nvim lua include paths
 local function include_rtpaths(fname, ext)
-  ext = ext or 'lua'
+  ext = ext or "lua"
   local sep = dir_separator()
   local rtpaths = vim.api.nvim_list_runtime_paths()
-  local modfile, initfile = fmt('%s.%s', fname, ext), fmt('init.%s', ext)
+  local modfile, initfile = fmt("%s.%s", fname, ext), fmt("init.%s", ext)
   for _, path in ipairs(rtpaths) do
     -- Look on runtime path for 'lua/*.lua' files
     local path1 = table.concat({ path, ext, modfile }, sep)
@@ -63,37 +63,37 @@ end
 -- Global function that searches the path for the required file
 function find_required_path(module)
   -- Look at package.config for directory separator string (it's the first line)
-  local sep = string.match(package.config, '^[^\n]')
+  local sep = string.match(package.config, "^[^\n]")
   -- Properly change '.' to separator (probably '/' on *nix and '\' on Windows)
-  local fname = vim.fn.substitute(module, '\\.', sep, 'g')
+  local fname = vim.fn.substitute(module, "\\.", sep, "g")
   local f
   ---- First search for lua modules
-  f = include_paths(fname, 'lua')
+  f = include_paths(fname, "lua")
   if f then
     return f
   end
   -- This part is just for nvim modules
-  f = include_rtpaths(fname, 'lua')
+  f = include_rtpaths(fname, "lua")
   if f then
     return f
   end
   ---- Now search for Fennel modules
-  f = include_paths(fname, 'fnl')
+  f = include_paths(fname, "fnl")
   if f then
     return f
   end
+
   -- This part is just for nvim modules
-  f = include_rtpaths(fname, 'fnl')
+  f = include_rtpaths(fname, "fnl")
   if f then
     return f
   end
 
   -- the file not found
-  vim.notify('File not found: ' .. module, vim.log.levels.ERROR)
-  return ''
+  vim.notify("File not found: " .. module, vim.log.levels.ERROR)
+  return ""
 end
 
 -- Set options to open require with gf
 vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
-vim.opt_local.includeexpr = 'v:lua.find_required_path(v:fname)'
-
+vim.opt_local.includeexpr = "v:lua.find_required_path(v:fname)"
--- after/ftplugin/vue.lua
+++ after/ftplugin/vue.lua
@@ -1,6 +1,5 @@
--- 
+--
 function gotofilevue(fname)
   return require("utils.functions").gotofile_vue(fname)
 end
 vim.opt.includeexpr = "v:lua.gotofilevue(v:fname)"
-
