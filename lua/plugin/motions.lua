4ad4313b34fea818e0115f3fe783efed735d16b2
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-14 21:22:00 -0500

    Feat: add motions

--- lua/plugin/motions.lua
+++ lua/plugin/motions.lua
@@ -22,4 +22,28 @@ return {
   --     "Files"
   --   }
   -- }
+
+  -- Flash
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      {"f"},
+      {"F"},
+      {"t"},
+      {"T"},
+      {";"},
+      {","},
+    },
+    config = require("plugin.config.flash_nvim")
+
+  }
 }
