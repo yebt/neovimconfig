23f0496ae0ab26ace05577b8ad3532ddc00b31d7
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-22 07:29:28 -0500

    Feat: init the lsp config

--- lua/plugin/php.lua
+++ lua/plugin/php.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "gbprod/phpactor.nvim",
-    event = "VeryLazy",
-    build = function()
-      require("phpactor.handler.update")()
-    end, -- To install/update phpactor when installing this plugin
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required to update phpactor
-      "neovim/nvim-lspconfig", -- required to automaticly register lsp serveur
-    },
-  },
+  -- {
+  --   "gbprod/phpactor.nvim",
+  --   event = "VeryLazy",
+  --   build = function()
+  --     require("phpactor.handler.update")()
+  --   end, -- To install/update phpactor when installing this plugin
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim", -- required to update phpactor
+  --     "neovim/nvim-lspconfig", -- required to automaticly register lsp serveur
+  --   },
+  -- },
 }
