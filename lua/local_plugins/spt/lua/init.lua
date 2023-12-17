268e361a5573512948a342ecd8f09819729fceb6
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-12-17 08:32:25 -0500

    Feat: add supertab plugin try

---
+++ lua/local_plugins/spt/lua/init.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+M.actions = {}
+
+-- callback (function)
+-- conditional (function)
+-- priority nil|(number)
+-- once nil|(boolean)
+-- opts nil|(table)
+M.suscribe = function(callback, conditional, priority, once,opts )
+  priority = priority or 0
+  once = once or false
+  opts = opts or {}
+  M.actions[#M.actions + 1] = {
+    callback = callback,
+    conditional = conditional,
+    called = false,
+    priority = priority,
+    onece = once,
+    opts = opts
+  }
+  table.sort(M.catons, function(a,b) return a.priority > b.priority end)
+end
+
+M.setup = function()
+  _G.supertabSuscribe = M.suscribe
+
+  local opts = {noremap = true, silent = true, expr = true}
+  vim.keymap.set ("i", "<tab>", function()
+    for key,value in pairs (M.actions)
+    end
+    return "<tab>"
+  end, opts)
+  
+end
+
+return M
