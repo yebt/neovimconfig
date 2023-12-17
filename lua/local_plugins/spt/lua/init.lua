e421226f065151d7a6bd492e533b12986060e462
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-12-17 08:50:53 -0500

    Feat: try implement the suscription call foreach in mapping and fix the opts call

--- lua/local_plugins/spt/lua/init.lua
+++ lua/local_plugins/spt/lua/init.lua
@@ -7,31 +7,43 @@ M.actions = {}
 -- priority nil|(number)
 -- once nil|(boolean)
 -- opts nil|(table)
-M.suscribe = function(callback, conditional, priority, once,opts )
-  priority = priority or 0
-  once = once or false
-  opts = opts or {}
-  M.actions[#M.actions + 1] = {
-    callback = callback,
-    conditional = conditional,
-    called = false,
-    priority = priority,
-    onece = once,
-    opts = opts
-  }
-  table.sort(M.catons, function(a,b) return a.priority > b.priority end)
+M.suscribe = function(callback, conditional, priority, once)
+	priority = priority or 0
+	once = once or false
+	-- opts = opts or {}
+	M.actions[#M.actions + 1] = {
+		callback = callback,
+		conditional = conditional,
+		called = false,
+		priority = priority,
+		onece = once,
+		-- opts = opts,
+	}
+	table.sort(M.catons, function(a, b)
+		return a.priority > b.priority
+	end)
 end
 
-M.setup = function(opts)
-  _G.supertabSuscribe = M.suscribe
+M.setup = function(config)
+	_G.supertabSuscribe = M.suscribe
 
-  local opts = {noremap = true, silent = true, expr = true}
-  vim.keymap.set ("i", "<tab>", function()
-    for key,value in pairs (M.actions) do
-    end
-    return "<tab>"
-  end, opts)
-  
+	local opts = { noremap = true, silent = true, expr = true }
+	vim.keymap.set("i", "<tab>", function()
+		for _, action in pairs(M.actions) do
+			if action.caleld and action.once then
+				break
+			end
+			if not (type(action.conditional) == "function" and action.conditional()) then
+				break
+			end
+			if not (type(action.conditional) =='boolean' and action.conditional) then
+				break
+			end
+			-- opts = vim.tbl_extend(opts, action.opts or {})
+			return action.callback()
+		end
+		return "<tab>"
+	end, opts)
 end
 
 return M
