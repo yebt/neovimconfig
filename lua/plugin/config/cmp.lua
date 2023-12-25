f5ab7570a2aac9a1ebb68070f3b7d7cd32fd4df4
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-12-24 19:21:36 -0500

    Feat: add changes inside the cmp

--- lua/plugin/config/cmp.lua
+++ lua/plugin/config/cmp.lua
@@ -1,6 +1,7 @@
 return function()
   local cmp = require("cmp")
   local types = require("cmp.types")
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
   local luasnip = require("luasnip")
 
@@ -21,7 +22,8 @@ return function()
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<CR>"] = cmp.mapping.confirm(), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
 
     snippet = {
@@ -29,5 +31,46 @@ return function()
         luasnip.lsp_expand(args.body)
       end,
     },
+
+    formatting = {
+      expandable_indicator = true,
+      fields = { "abbr", "kind", "menu" },
+      format = function(_, vim_item)
+        return vim_item
+      end,
+    },
+    matching = {
+      disallow_fuzzy_matching = false,
+      disallow_fullfuzzy_matching = false,
+      disallow_partial_fuzzy_matching = true,
+      disallow_partial_matching = false,
+      disallow_prefix_unmatching = false,
+    },
+    sources = {},
+
+    confirmation = {
+      default_behavior = types.cmp.ConfirmBehavior.Insert,
+      get_commit_characters = function(commit_characters)
+        return commit_characters
+      end,
+    },
+
+    experimental = {
+      ghost_text = false,
+    },
+
+    view = {
+      entries = {
+        name = "custom",
+        selection_order = "top_down",
+      },
+      docs = {
+        auto_open = true,
+      },
+    },
   })
+  --
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+
 end
