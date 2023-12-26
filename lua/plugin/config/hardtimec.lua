43e73cf8a64e7f7d7b1ce4013b501a376c7f6cd4
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-12-26 10:55:36 -0500

    Feat: add condfig to hardtime

---
+++ lua/plugin/config/hardtimec.lua
@@ -0,0 +1,45 @@
+return function ()
+  local ok, hardtime = pcall(require, "hardtime")
+  if not ok then
+    return
+  end
+
+  local opts =  {
+    disable_mouse = false,
+
+    -- Add "oil" to the disabled_filetypes
+    disabled_filetypes = {
+      "NvimTree",
+      "TelescopePrompt",
+      "aerial",
+      "alpha",
+      "checkhealth",
+      "dapui-repl",
+      "dapui_breakpoints",
+      "dapui_console",
+      "dapui_scopes",
+      "dapui_stacks",
+      "dapui_watches",
+      "DressingInput",
+      "DressingSelect",
+      "help",
+      "lazy",
+      "NeogitStatus",
+      "NeogitLogView",
+      "mason",
+      "neotest-summary",
+      "minifiles",
+      "neo-tree",
+      "neo-tree-popup",
+      "netrw",
+      "noice",
+      "notify",
+      "prompt",
+      "qf",
+      "oil",
+      "undotree",
+   },
+
+  }
+  hardtime.setup(opts)
+end
